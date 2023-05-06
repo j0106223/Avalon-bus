eb9f1172939c3f639aa57562bc71669fb2dfaa35
jack <j0106223@gmail.com>
2023-05-07 00:26:44 +0800

    add HDL_Library may be I will rename it as IP Library...

--- Avalon_ip/Avalon_slave/PS2/ps2_core.v
+++ Avalon_ip/Avalon_slave/PS2/ps2_core.v
@@ -4,16 +4,39 @@ module ps2_core (
     rdata_i,
     wdata_o,
     ps2_clk,
-    ps2_data,
+    ps2_data_i,
+    ps2_data_o,
 );
-    input clk;
-    input reset_n;
-    input rdata_i;
+    input   clk;
+    input   reset_n;
+    input   ps2_clk;
+    input   ps2_data_i;
+    output  ps2_data_o;
+    input   rdata_i;
     always @(posedge clk or negedge reset_n) begin
         if(!reset_n)begin
 
         end else begin
             
+        end
+    end
+
+    //sync ps2 clock
+    wire ps2_clk_sync;
+    reg [1:0]ps2_clk_q;
+    always @(posedge clk or negedge reset_n) begin
+        if(!reset_n)begin
+            ps2_clk_q <= 2'b00;
+        end else begin
+            ps2_clk_q[0] <= ps2_clk;
+            ps2_clk_q[1] <= ps2_clk_q[0];
+        end
+    end
+    //sync ps2 data
+    reg [1:0]ps2_data_i_q;
+    always @(posedge clk or negedge reset_n) begin
+        if(!reset_n)begin
+        end else begin
         end
     end
 endmodule
---
+++ Avalon_ip/HDL_Library/synchronizer.v
@@ -0,0 +1,24 @@
+`default_nettype none
+module synchronizer(
+    clk,
+    reset_n,
+    in,
+    out
+);
+//instance example :
+//synchronizer (.clk(clk), reset_n(reset_n), in(), out());    
+    input  clk;
+    input  reset_n;
+    input  in;
+    output out;
+    reg [1:0]q;
+    assign out = q[1];
+    always @(posedge clk or negedge reset_n) begin
+        if(!reset_n)begin
+            q <= 2'b00;
+        end else begin
+            q[0] <= in;
+            q[1] <= q[0];
+        end
+    end
+endmodule
