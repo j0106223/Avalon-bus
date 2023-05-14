e334cdb19268ec4a6decb86e3a9a6d7fded6ca95
Jack <j0106223@gmail.com>
2023-05-14 23:23:10 +0800

    add port list of i2s avalon interface and i2s io definition

--- Avalon_ip/Avalon_slave/I2S/i2s_core.v
+++ Avalon_ip/Avalon_slave/I2S/i2s_core.v
@@ -3,5 +3,11 @@ module i2s_core (
     reset_n,
 );
     input clk;
-    input reset_n;    
+    input reset_n;
+    input [7:0]data_right;//right channel
+    input [7:0]data_left;//left channel
+    output scl;
+    output sda;//one way transaction
+    output ws;//left right channel
+
 endmodule
