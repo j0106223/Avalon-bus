177542e95536ffd9057fe23c8ebed48114794a65
Jack <j0106223@gmail.com>
2023-05-15 23:40:56 +0800

    move function definition to c file

---
+++ CPU_smulator/src/control.c
@@ -0,0 +1,24 @@
+#include "../inc/control.h"
+
+void control_decode(struct control *control_inst, int opcode){
+    if(){
+        control_inst->ALUOp = ;
+    }
+    if(){
+
+    }
+    
+    control_inst->ALUSrc;
+    control_inst->Branch;
+    control_inst->MemRead;
+    control_inst->MemtoReg;
+    control_inst->MemWrite;
+    control_inst->RegWrite;
+    switch (opcode){
+    case 0:/* constant-expression */:
+        /* code */
+        break;
+    default:
+        break;
+    }
+}
