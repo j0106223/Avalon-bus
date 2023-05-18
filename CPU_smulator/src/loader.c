40e5aa8f8acd9f642b0d07872c889e5f35e9a4d9
Jack <j0106223@gmail.com>
2023-05-18 22:33:30 +0800

    ihex format decode, get record type, adding another way to initialize memory by using txt which can be generated by some online assembler

--- CPU_smulator/src/loader.c
+++ CPU_smulator/src/loader.c
@@ -7,9 +7,40 @@ void hex2mem(char *file_name, struct memory *rom,struct memory *ram){
     FILE *fp;
     int start_addr;
     int end_addr;
-    fp = fopen(file_name,"r");
-    write_mem(&rom, addr, data);
+    char line_text[50];
+    fp = fopen(file_name, "r");
+    int type;
+    while(fscanf(fp, "%s", line_text) != EOF){
+        switch(line_text[7]){//record type
+            case '0'://data
+                ;
+                break;
+            case '1'://eof
+                ;
+                break;
+            case '2'://Extended Segment Address
+                ;
+                break;
+            case '3'://Start Segment Address
+                ;
+                break;
+            case '4'://Extended Linear Address
+                ;
+                break;
+            case '5'://Start Linear Address
+                ;
+                break;
+        }
+    }
     fclose(fp);
+}
+void txt2mem(char *file_name, struct memory *rom){
+    FILE *fp;
+    fp = fopen(file_name,"r");
+    int data;
+    for(int i = 0; fscanf(fp,"%X", &data) != EOF; i++){
+        rom->data[i] = data;//little endian check
+    }
 }
 int check_memory_region(int load_addr, struct memory *mem){
     if((addr < mem->base) || (addr > (mem->base + mem->size))){
