051a5f42217f53e673f08488d030a958198107fd
jack <j0106223@gmail.com>
2023-05-20 01:07:51 +0800

    register abi name

--- CPU_smulator/inc/regfile.h
+++ CPU_smulator/inc/regfile.h
@@ -4,4 +4,16 @@
 struct regfile{
     int x[REG_NUM];//need parameterize
 };
+char regABI_NAME[][5] = {
+    "zero",
+    "ra",
+    "sp",
+    "gp",
+    "tp",
+    "t0", "t1", "t2",
+    "s0/fp", "s1",
+    "a0", "a1", "a2", "a3", "a4", "a5", "a6", "a7",
+    "s2", "s2", "s4", "s5", "s6", "s7", "s8", "s9", "s10", "s11",
+    "t3", "t4", "t5", "t6"
+};
 #endif
--- CPU_smulator/src/debug.c
+++ CPU_smulator/src/debug.c
@@ -1,4 +1,6 @@
 #include "../inc/debug.h"
+#include "../inc/cpu.h"
+#include <stdio.h>
 void show32bit(unsigned int data){
     int i;
     for(i = 0; i < 32; i++){  
@@ -18,4 +20,12 @@ void showNbit(unsigned int data,int n){
             printf("0");
         }      
     }
+}
+void showRegs(struct regfile *regfile_inst){
+    int i;
+    printf("=================Register File Content=================");
+    for(i = 0; i < REG_NUM; i++){
+        printf("x%d/%s = %X%d \t", i, reg_name[i], regfile_inst->x[i]);
+    }
+    printf("=================Register File Content=================");
 }
