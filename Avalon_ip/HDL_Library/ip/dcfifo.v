a2555248ac7602f5919634b8c6c26bc78c2c8d8d
Jack <j0106223@gmail.com>
2023-06-07 23:50:30 +0800

    complete dcfifo and make synchronizer module parameterize

--- Avalon_ip/HDL_Library/ip/dcfifo.v
+++ Avalon_ip/HDL_Library/ip/dcfifo.v
@@ -1,44 +1,55 @@
 module dcfifo(
     
 );
-    parameter W_WIDTH = 32;
-    parameter R_WIDTH = 32;
+    parameter WIDTH = 32;
     parameter DEPTH = 32;
     input wclk;
     input rclk;
     input reset_n;
     input write;
     input read;
-    output wvalid;
-    output rvalid;
-    input  wdata;
-    output rdata;
+    input          [WIDTH-1:0]wdata;
+    output         [WIDTH-1:0]rdata;
     output full;
     output empty;
-    reg [W_WIDTH-1:0] data [0:DEPTH-1];
-    reg [$clog2(W_WIDTH)-1:0]widx;
-    reg []ridx;
-    integer i;
-    assign full = ;
-    assign empty = ;
+    output [$clog2(WIDTH)-1:0] used;
+    wire   [$clog2(WIDTH)-1:0] wptr_sync;
+    wire   [$clog2(WIDTH)-1:0] rptr_sync;
+    reg    [$clog2(WIDTH)-1:0] wptr;
+    reg    [$clog2(WIDTH)-1:0] rptr;
+    reg            [WIDTH-1:0] data [0:DEPTH-1];
+    assign used = (wptr < rptr_sync) ?
+                  (wptr + DEPTH - rptr_sync):
+                  (wptr - rptr_sync);
+    assign full = (wptr < rptr_sync) ?
+                  ((wptr + DEPTH - rptr_sync) == (DEPTH - 1)):
+                  ((wptr - rptr_sync) == (DEPTH - 1));
+    assign empty = (rptr == wptr_sync);
+    
+    always @(posedge wclk or negedge reset_n) begin
+        if(write && !full)begin
+            data[wptr] <= wdata;
+        end
+    end
     always @(posedge wclk or negedge reset_n) begin
         if(!reset_n)begin//just init idx pointer 
-            
+            wptr <= 0;
         end else begin
-            if(write && !full)begin
-
+            if(write && (!full))begin
+                wptr <= wptr + 1'b1;
             end
         end
     end
     always @(posedge wclk or negedge reset_n) begin
         if(!reset_n)begin//just init idx pointer 
-            
+            rptr <= 0;
         end else begin
-            if(read && !empty)begin
-
-            end
+            if(read && (!empty))begin
+                rptr <= rptr + 1'b1;
+            end            
         end
     end
-    synchronizer wsync(.clk(wclk), reset_n(reset_n), in(), out());
-    synchronizer rsync(.clk(rclk), reset_n(reset_n), in(), out());
+
+    synchronizer wsync(.clk(wclk), reset_n(reset_n), in(wptr), out(wptr_sync));
+    synchronizer rsync(.clk(rclk), reset_n(reset_n), in(rptr), out(rptr_sync));
 endmodule
--- Avalon_ip/HDL_Library/synchronizer.v
+++ Avalon_ip/HDL_Library/synchronizer.v
@@ -5,19 +5,21 @@ module synchronizer(
     in,
     out
 );
+parameter WIDTH = 1;
 //instance example :
 /*
-synchronizer (.clk(clk), reset_n(reset_n), in(), out());
+synchronizer #(.WIDTH()) inst(.clk(clk), reset_n(reset_n), in(), out());
 */    
     input  clk;
     input  reset_n;
     input  in;
     output out;
-    reg [1:0]q;
+    reg [WIDTH-1:0]q[0:1];
     assign out = q[1];
     always @(posedge clk or negedge reset_n) begin
         if(!reset_n)begin
-            q <= 2'b00;
+            q[0] <= 0;
+            q[1] <= 0;
         end else begin
             q[0] <= in;
             q[1] <= q[0];
