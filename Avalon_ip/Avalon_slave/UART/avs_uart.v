2eee56b83447a5fa88b1a18b1c000eb54b603b28
jack <j0106223@gmail.com>
2023-06-17 18:20:56 +0800

    avalon uart slave interface internal registers read write logic

--- Avalon_ip/Avalon_slave/UART/avs_uart.v
+++ Avalon_ip/Avalon_slave/UART/avs_uart.v
@@ -8,80 +8,87 @@ module avs_uart (
     avs_s0_readdata,
     avs_s0_writedata
 );
-    reg [7:0]rx_data;
-    reg [7:0]tx_data;
-    reg [7:0]control;
-    reg [7:0]status;
-    reg [31:0]clk_div;
+    input          clk;
+    input          reset_n;
+    input    [2:0] avs_s0_address;
+    input          avs_s0_read;
+    input          avs_s0_write;
+    output         avs_s0_waitrequest;
+    output  [31:0] avs_s0_readdata;
+    input   [31:0] avs_s0_writedata;
+
+    reg  [7:0] rx_data; //reg[0]
+    reg  [7:0] tx_data; //reg[1]
+    reg  [7:0] control; //reg[2]
+    reg  [7:0] status;  //reg[3]
+    reg [31:0] clk_div; //reg[4]
+    wire tx_data_we = avs_s0_write & (avs_s0_address == 3'd1);
+    wire control_we = avs_s0_write & (avs_s0_address == 3'd2);
+    wire clk_div_we = avs_s0_write & (avs_s0_address == 3'd4);
     always @(posedge clk or negedge reset_n) begin
         if(!reset_n)begin
             tx_data <= 0;
             control <= 0;
-            status  <= 0;
             clk_div <= 0;
         end else begin
             if(avs_s0_write)begin
                 case (avs_s0_address)
                     1: tx_data <= avs_s0_writedata;
                     2: control <= avs_s0_writedata;
-                    3: status  <= avs_s0_writedata;
                     4: clk_div <= avs_s0_writedata;
                 endcase
             end
         end
     end
-
+//============update register================
     always @(posedge clk or negedge reset_n) begin
         if(!reset_n)begin
             status<=0;
         end else begin
-            if()begin
-            end else begin
+            if(uart_core_rx_done)begin
+                rx_data <= uart_core_rx_data;
             end
         end
     end
-    reg [7:0]readdate;
-    assign avs_s0_readdata = {24'b0, readdate};
-    always @(*) begin
-        readdate = 0;
+    
+    /*always @(*) begin
         if(avs_s0_read)begin
             case (avs_s0_address)
-                0: avs_s0_readdata = {24'b0, rx_data};
-                1: avs_s0_readdata = {24'b0, tx_data};
-                2: avs_s0_readdata = {24'b0, control};
-                3: avs_s0_readdata = {24'b0, status};
-                4: avs_s0_readdata = clk_div;
+                0: readdata = {24'b0, rx_data};
+                1: readdata = {24'b0, tx_data};
+                2: readdata = {24'b0, control};
+                3: readdata = {24'b0, status};
+                4: readdata = clk_div;
                 default: readdate = 0;
             endcase
         end else begin
             readdate = 0;
         end
     end
+*/
 
-
-    wire uart_clk;
-    wire rx_clk;
-    wire tx_clk;
-    assign rx_clk = uart_clk;
-    assign tx_clk = uart_clk;
+    wire uart_core_clk;
+    wire uart_core_rx_clk;
+    wire uart_core_tx_clk;
+    assign uart_core_rx_clk = uart_core_clk;
+    assign uart_core_tx_clk = uart_core_clk;
     clk_gen uart_clk_gen(
         .clk_i   (clk),
         .reset_n (reset_n),
         .clk_div (clk_div),
-        .clk_o   (uart_clk)
+        .clk_o   (uart_core_clk)
     );
-
-    uart_core uart_core_inst(
+    uart_core inst(
         .reset_n    (reset_n),
-        .rx_clk     (rx_clk),
-        .rx_data    (rx_data),
-        .rx_done    (rx_done),
-        .rx         (rx),
-        .tx_clk     (tx_clk),
-        .tx_valid   (tx_valid),
-        .tx_data    (tx_data),
-        .tx_ready   (tx_ready),
-        .tx_done    (tx_done),
-        .tx         (tx)
+        .rx_clk     (uart_core_rx_clk),
+        .rx_data    (uart_core_rx_data),
+        .rx_done    (uart_core_rx_done),
+        .rx         (uart_core_rx),
+        .tx_clk     (uart_core_tx_clk),
+        .tx_valid   (uart_core_tx_valid),
+        .tx_data    (uart_core_tx_data),
+        .tx_ready   (uart_core_tx_ready),
+        .tx_done    (uart_core_tx_done),
+        .tx         (uart_core_tx)
     );
 endmodule
---
+++ Avalon_ip/Avalon_slave/UART/testbench/avs_uart_tb/avs_uart_tb.v
@@ -0,0 +1,6 @@
+module avs_uare_tb;
+    
+
+
+    
+endmodule
