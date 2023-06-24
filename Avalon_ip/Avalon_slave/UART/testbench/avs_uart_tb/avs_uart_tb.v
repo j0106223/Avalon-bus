0d995e570f08bb139ad50578437cf476dc03792c
jack <j0106223@gmail.com>
2023-06-25 00:34:38 +0800

    build avalon uart testbench and create some read write task

--- Avalon_ip/Avalon_slave/UART/testbench/avs_uart_tb/avs_uart_tb.v
+++ Avalon_ip/Avalon_slave/UART/testbench/avs_uart_tb/avs_uart_tb.v
@@ -1,6 +1,53 @@
 module avs_uare_tb;
-    
+    reg clk;
+    reg reset_n;
+    initial begin
+        $dumpfile("avs_uare_tb.vcd");
+        $dumpvars(0, avs_uare_tb);    
+    end
+    initial begin
+        clk = 0;
+        forever begin
+           #1 clk = ~clk; 
+        end
+    end
+    initial begin
+           reset_n = 1'b1;
+        #1 reset_n = 1'b0;
+        #1 reset_n = 1'b1;
+        //pattern
+
+    end
 
 
     
+    //dut inst
+    task avm_read;
+        begin
+        end
+    endtask
+    task avm_write;
+        begin
+        end
+    endtask
+    task set_tx_data;
+        begin
+        end
+    endtask
+    task set_control;
+        begin
+        end
+    endtask
+    task set_clk_div;
+        begin
+        end
+    endtask
+    task get_rx_data;
+        begin
+        end
+    endtask
+    task get_status;
+        begin
+        end
+    endtask
 endmodule
