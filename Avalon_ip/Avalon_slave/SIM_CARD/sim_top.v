7c88e225586a6026b7d6fb56aeb1475c57cedc8b
Jack <j0106223@gmail.com>
2023-07-04 20:50:57 +0800

    add simcard related design

---
+++ Avalon_ip/Avalon_slave/SIM_CARD/sim_top.v
@@ -0,0 +1,16 @@
+module sim_top (
+    clk,
+    reset_n,
+    sim_clk,
+    sim_reset_n,
+    sim_io
+);
+
+    input  clk;
+    input  reset_n;
+    output sim_clk;
+    output sim_reset_n;
+    inout  sim_io;
+
+      
+endmodule
