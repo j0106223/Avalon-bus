cf3cedee1f1d3698205f8bb77a4f98cf97831c5d
jack <j0106223@gmail.com>
2023-07-30 21:37:43 +0800

    add ahb_write task

--- Avalon_ip/HDL_Library/vip/ahb_master/ahb_master.v
+++ Avalon_ip/HDL_Library/vip/ahb_master/ahb_master.v
@@ -10,25 +10,36 @@ module ahb_master (
 );
     parameter DW = 32;
     parameter AW = 32;
-    input           hclk;
-    input           hreset_n;
-    output [AW-1:0] haddr;
-    output    [1:0] htrans;
-    output    [2:0] hsize;
-    output          hwrite;
-    output [DW-1:0] hwdata;
-    input  [DW-1:0] hrdata;
-    input           hready;
+    input               hclk;
+    input               hreset_n;
+    output reg [AW-1:0] haddr;
+    output reg    [1:0] htrans;
+    output reg    [2:0] hsize;
+    output reg          hwrite;
+    output reg [DW-1:0] hwdata;
+    input      [DW-1:0] hrdata;
+    input               hready;
 
     initial begin
         
     end
-    
+
     task ahb_write(
-        input addr,
-        input data
+        input [AW-1:0]addr,
+        input [DW-1:0]data
     );
-        begin
+        begin  
+            @(posedge hclk)begin
+                haddr = addr;
+            end
+            @(posedge hclk)begin
+                hwdata = data;
+            end
+            @(posedge hclk)begin
+                wait(hready);
+            end
+            @(posedge hclk);
+            hwdata = {DW{1'bx}};
         end
     endtask
 
