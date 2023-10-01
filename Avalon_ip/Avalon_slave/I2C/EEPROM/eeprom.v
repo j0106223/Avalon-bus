33acd2b24f11b6674d2c31ed0c3c28c1a8612fb3
jack <j0106223@gmail.com>
2023-10-01 16:52:57 +0000

    eeprom fsm coding

--- Avalon_ip/Avalon_slave/I2C/EEPROM/eeprom.v
+++ Avalon_ip/Avalon_slave/I2C/EEPROM/eeprom.v
@@ -3,7 +3,8 @@ module eeprom (
 );
     parameter PB = ;
     reg [7:0] mem [0:7];
-
+    reg page_buffer;
+    
     mem[x] = xxx;
 
     mem_page (
--- Avalon_ip/Avalon_slave/I2C/EEPROM/fsm.v
+++ Avalon_ip/Avalon_slave/I2C/EEPROM/fsm.v
@@ -8,13 +8,51 @@ module fsm (
     input sda;
     input start;
     input stop;
+    output [7:0]data_word;
+    output data_load;
     reg [3:0]state;
-    reg [3:0]state_next;
-    localparam IDLE = 1;
-    //localparam IDLE = 1;
-    //localparam IDLE = 1;
-    //localparam IDLE = 1;
-    always @(negedge scl) begin
-        
+    reg [3:0]next_state;
+    localparam STOP = 0;
+    localparam ID6 = 1;
+    localparam ID5 = 2;
+    localparam ID4 = 3;
+    localparam ID3 = 4;
+    localparam ID2 = 5;
+    localparam ID1 = 6;
+    localparam ID0 = 7;
+    localparam ID_ACK = 1;
+    localparam RA7 = 1;
+    localparam RA6 = 1;
+    localparam RA5 = 1;
+    localparam RA4 = 1;
+    localparam RA3 = 1;
+    localparam RA2 = 1;
+    localparam RA1 = 1;
+    localparam RA0 = 1;
+    
+    
+    
+    
+    
+    
+    localparam RA_ACK = 1;
+
+    always @(negedge scl or posedge stop) begin
+        if(start)begin
+            state <= stop;
+        end else begin
+            state <= next_state;
+        end
+    end
+
+    always @* begin
+        case (state)
+            : next_state;
+            default: 
+        endcase
+    end
+
+    always @(posedge scl) begin
+        data_word <= {data_word, sda};
     end
 endmodule
