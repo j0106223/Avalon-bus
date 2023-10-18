a89ac0d124910b297c9c6b44a045a85449ec204b
jack <j0106223@gmail.com>
2023-10-18 16:11:16 +0000

    eeprom module and addr_and_cnt module design are finished

--- Avalon_ip/Avalon_slave/I2C/EEPROM/addr_and_cnt.v
+++ Avalon_ip/Avalon_slave/I2C/EEPROM/addr_and_cnt.v
@@ -1,39 +1,32 @@
-module addr_and_cnt (
+module addr_and_cnt (//address and counter about the data word whitch should be read out or write in
     scl,
 	sda,
 	addr_load,
     inc,
-
+	addr
 );
-    input addr_load;
-    input inc;
-    output data_word;
-    output [4:0] row;
-    output [2:0] col;
-    reg wptr
-	reg [7:0] data_word;
-	reg [7:0] address;
-	reg [2:0] cnt;
-
-
+	input        scl;
+	input        sda;
+    input        addr_load;
+    input        inc;
+    output [7:0] addr;
+	
     reg  [7:0] shift_reg;
     always @(posedge scl) begin
     	shift_reg <= {shift_reg[7:1], sda};
     end
 
+	reg [4:0] address;
+	reg [2:0] cnt;
     always @(posedge scl) begin
 		if (addr_load) begin
-			cnt <= 3'b000;
+			address <= shift_reg[7:3];
+			cnt     <= shift_reg[2:0];
 		end else begin
-			cnt <= cnt + 1'b1;
+			if(inc)begin
+				cnt <= cnt + 1'b1;
+			end
 		end
     end
-
-	always @(posedge scl) begin
-		if (rw) begin
-			data_word <= data
-		end else begin
-			address <= data;
-		end
-	end
+	assign addr = {address, cnt};
 endmodule
--- Avalon_ip/Avalon_slave/I2C/EEPROM/eeprom.v
+++ Avalon_ip/Avalon_slave/I2C/EEPROM/eeprom.v
@@ -1,43 +1,20 @@
 module eeprom (
-    clk,
+    scl,
+    addr,
     write,
-    row,
-    col,
     data_i,
 	data_o
 );
-    parameter PAGE_NUM = 32;
-    parameter PAGE_SIZE = BYTE_NUM * 8 = 64;
     input clk;
     input write;
-    input  [4:0] row;
-    input  [2:0] col;
-	input  [PAGE_SIZE-1:0] data_i;
+    input  [7:0] addr;
+	input  [7:0] data_i;
 	output [7:0] data_o;
-    assign data_o = data_q[row];
-    wire [PAGE_SIZE-1:0] data_q [0:PAGE_NUM-1];
-
-    wire [PAGE_NUM-1:0] page_cs;
-    genvar i;
-    generate
-        for(i = 0;i <  PAGE_NUM;i = i + 1)begin:cs_gen
-            assign page_cs[i] = (i == row); 
+    reg [7:0] mem [0:255];
+    always @(posedge scl) begin//may be negedge
+        if(write)begin
+            mem[addr] <= data_i;
         end
-        for (i = 0; i < PAGE_NUM; i = i + 1) begin:mem_gen
-            mem_page mem_inst(
-                .clk    (clk),//STOP PULSE SIGNAL
-                .cs     (page_cs[i]),
-                .write  (write),
-                .data_i (data_i),
-                .data_o (data_q[i])
-            );
-        end
-    endgenerate
-
-    reg [7:0] page_buffer[0:7];
-    always @(posedge scl) begin
-        page_buffer[0]<=[];
     end
-    page_buffer = {};
-    [63:0] page_buffer
+    assign data_o = mem[addr];
 endmodule
